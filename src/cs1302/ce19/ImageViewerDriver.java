5e3b93878440bf2dff481bc61251b8ff6e1d8d25
Michael Cotterell <mepcotterell@gmail.com>
2019-03-06 17:41:29 -0500

    Update ImageViewerDriver.java

--- src/cs1302/ce19/ImageViewerDriver.java
+++ src/cs1302/ce19/ImageViewerDriver.java
@@ -6,7 +6,7 @@ public class ImageDriver {
 
     public static void main(String[] args) {
         try {
-            Application.launch(ImageViewerApp.class, args);
+            Application.launch(ImageApp.class, args);
         } catch (Exception e) {
             System.err.println(e);
             System.err.println("Likely due to X11 timeout. Logout and log back in...");
