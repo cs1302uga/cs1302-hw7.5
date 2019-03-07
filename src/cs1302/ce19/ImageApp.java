3dbaaef65ef80fe1497fe9bf1af109ffd10d0570
Michael Cotterell <mepcotterell@gmail.com>
2019-03-07 07:49:54 -0500

    Update ImageApp.java

--- src/cs1302/ce19/ImageApp.java
+++ src/cs1302/ce19/ImageApp.java
@@ -1,14 +1,9 @@
 package cs1302.ce19;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
+import javafx.scene.layout.VBox;
 
 /** 
  * Represents a basic image viewer app.
