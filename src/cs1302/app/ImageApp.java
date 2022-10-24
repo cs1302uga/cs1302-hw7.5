83a74f9f0db879293dd87c79dbcea6b69e60edf2
Brad Barnes <bjb211@uga.edu>
2022-10-24 17:58:44 -0400

    Removed import statements

--- src/cs1302/app/ImageApp.java
+++ src/cs1302/app/ImageApp.java
@@ -4,11 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
 /**
  * Represents a basic image viewer app.
