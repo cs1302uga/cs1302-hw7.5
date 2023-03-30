879c2cbad125650689e5394d77d3cde8aba5bdc9
Michael Cotterell <mepcotterell@gmail.com>
2023-03-30 10:13:35 -0400

    adjusted compile script

--- compile.sh
+++ compile.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -ex
 
-# Compile the app
-javac -d bin -p $JAVAFX_HOME/lib --add-modules javafx.controls src/cs1302/app/ImageApp.java
-javac -d bin -cp bin -p $JAVAFX_HOME/lib --add-modules javafx.controls src/cs1302/app/ImageDriver.java
+# compile src/cs1302/app/ImageApp.java
+javac -d bin \
+      -p $JAVAFX_HOME/lib --add-modules javafx.controls \
+      src/cs1302/app/ImageApp.java
 
-# Run the app
-java -cp bin -Dprism.order=sw -p $JAVAFX_HOME/lib --add-modules javafx.controls cs1302.app.ImageDriver
+# compile src/cs1302/app/ImageDriver.java
+javac -d bin -cp bin \
+      -p $JAVAFX_HOME/lib --add-modules javafx.controls \
+      src/cs1302/app/ImageDriver.java
+
+# run the app using cs1302.app.ImageDriver
+java -cp bin -Dprism.order=sw \
+     -p $JAVAFX_HOME/lib --add-modules javafx.controls \
+     cs1302.app.ImageDriver
